b9174030a9d8f04d95e837daf044fb62c787869e
xiaomineu <zhangminneu@sina.com>
2019-05-24 20:11:22 +0800

    JumpGameJudge-55

--- src/JumpGameJudge/canJump.java
+++ src/JumpGameJudge/canJump.java
@@ -1,5 +1,20 @@
 package JumpGameJudge;
 
 public class canJump {
-    
+    public static void main(String[] args) {
+        int[] nums={2,0,0};
+        System.out.println(Judge(nums));
+    }
+    private static boolean Judge(int[] nums){
+        int maxIndex=0;
+        for(int i=0;i<nums.length;i++){
+            if(i<=maxIndex){
+                maxIndex= Math.max(nums[i]+i,maxIndex);
+            }
+        }
+        if(maxIndex>=nums.length-1){
+            return true;
+        }
+        return false;
+    }
 }
